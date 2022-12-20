5763576d1a0e282b474d4422b06754983a2a9c02
ArpitJoseph <66740333+ArpitJoseph@users.noreply.github.com>
2022-12-20 18:42:27 +0530

    Update FeedBack.swift

--- Sources/Genex-iOS/FeedBack.swift
+++ Sources/Genex-iOS/FeedBack.swift
@@ -16,56 +16,56 @@ class FeedBack {
     private var feedbackAnglesTable: String
 
     
-    init {
-        val homePath = System.getProperty("user.dir")
-        println("Working Directory = $homePath")
+    init() {
+//        var homePath = System.getProperty("user.
         
         // Load Important Angles
         var fileName = "Important Angles Sheet.csv"
-        println("File Name = $fileName")
+        
         try {
             //            val fileReader = BufferedReader(FileReader("$homePath/$fileName"))
-            val fileReader = context.assets.open(fileName).bufferedReader()
+            var fileReader = context.assets.open(fileName).bufferedReader()
             
             while (true) {
-                val line = fileReader.readLine() ?: break
-                val tokens = line.split(",")
+                var line = fileReader.readLine()
+                var tokens = line.split(",")
                 impAnglesTable[mutableListOf(tokens[0])] = mutableListOf(tokens[1])
             }
-            Log.d("GenexSDK", impAnglesTable.toString())
-        } catch (e: Exception) {
-            Log.e("genex_android_feedback", e.printStackTrace().toString())
+            debugPrint("GenexSDK", impAnglesTable)
+        } catch (e) {
+            debugPrint("genex_android_feedback")
         }
         
     }
     // Load Feedback
-          fileName = "Feedback Sheet.csv"
-          println("File Name = $fileName")
+         var fileName = "Feedback Sheet.csv"
+          
           try {
   //            val fileReader = BufferedReader(FileReader("$homePath/$fileName"))
-              val fileReader = context.assets.open(fileName).bufferedReader()
+              var fileReader = context.assets.open(fileName).bufferedReader()
 
               while (true) {
-                  val line = fileReader.readLine() ?: break
-                  val tokens = line.split(",")
+                  var line = fileReader.readLine() ?: break
+                  var tokens = line.split(",")
                   feedbackTable[mutableListOf(tokens[0], tokens[1], tokens[2])] =
                       mutableListOf(tokens[3], tokens[4])
               }
               Log.d("GenexSDK", feedbackTable.toString())
+              
           } catch (e: Exception) {
               Log.e("genex_android_feedback", e.printStackTrace().toString())
           }
 
           // Load Feedback Angles
-          fileName = "Feedback Angles Sheet.csv"
-          println("File Name = $fileName")
+          var fileNames = "Feedback Angles Sheet.csv"
+        
           try {
   //            val fileReader = BufferedReader(FileReader("$homePath/$fileName"))
-              val fileReader = context.assets.open(fileName).bufferedReader()
+              var fileReader = context.assets.open(fileName).bufferedReader()
 
               while (true) {
-                  val line = fileReader.readLine() ?: break
-                  val tokens = line.split(",")
+                  var line = fileReader.readLine() ?: break
+                  var tokens = line.split(",")
                   feedbackAnglesTable[mutableListOf(tokens[0], tokens[1], tokens[2], tokens[3])] =
                       mutableListOf(tokens[4])
               }
@@ -75,65 +75,62 @@ class FeedBack {
           }
       }
 
-      /**
-       * Helper function that returns feedback for a particular exercise, side and pose sequence
-       * */
-      private fun getFeedback(
+     
+      private func getFeedback(
           poseSeq: PoseSequence,
           exName: String,
           side: String,
           keypoint: String
-      ): Utils.CustomFeedbackClass {
+      ) -> Utils.CustomFeedbackClass {
 
   //        Make angles from pose sequence
-          val ps = getJointIndexes(keypoint.toInt(), side)
-          val angles = getJointAngles(poseSeq, ps)
+          var ps = getJointIndexes(keypoint.toInt(), side)
+          var angles = getJointAngles(poseSeq, ps)
 
   //        Initialize ideal min and max angles for the given setup
-          val minIdealAngle =
+          var minIdealAngle =
               feedbackAnglesTable[mutableListOf(exName, side, keypoint, "min")]?.get(0)?.toFloat()
-                  ?: 180.0f
-          val maxIdealAngle =
+                  
+          var maxIdealAngle =
               feedbackAnglesTable[mutableListOf(exName, side, keypoint, "max")]?.get(0)?.toFloat()
-                  ?: 0.0f
 
   //        Find min and max angles from the given pose sequence
-          val minAngle = angles.minOrNull() ?: minIdealAngle
-          val maxAngle = angles.maxOrNull() ?: maxIdealAngle
+          var minAngle = angles.minOrNull()
+          var maxAngle = angles.maxOrNull()
 
   //        Logic for giving feedback
           if (minAngle < (minIdealAngle - Constants.FEEDBACK_EASINESS)) { // D1
-              val feedback = feedbackTable[mutableListOf(exName, keypoint, "D1")]
+              var feedback = feedbackTable[mutableListOf(exName, keypoint, "D1")]
               return Utils.CustomFeedbackClass(
-                  feedback?.get(0) ?: "No Feedback",
-                  feedback?.get(1) ?: "0.mp3",
+                  feedback?.get(0),
+                  feedback?.get(1),
                   isFormDetected = true,
                   isFormCorrect = false
               )
           }
           if (minAngle > (minIdealAngle - Constants.FEEDBACK_EASINESS)) { // D2
-              val feedback = feedbackTable[mutableListOf(exName, keypoint, "D2")]
+              var feedback = feedbackTable[mutableListOf(exName, keypoint, "D2")]
               return Utils.CustomFeedbackClass(
-                  feedback?.get(0) ?: "No Feedback",
-                  feedback?.get(1) ?: "0.mp3",
+                  feedback?.get(0),
+                  feedback?.get(1),
                   isFormDetected = true,
                   isFormCorrect = false
               )
           }
           if (maxAngle < (maxIdealAngle - Constants.FEEDBACK_EASINESS)) { // U1
-              val feedback = feedbackTable[mutableListOf(exName, keypoint, "U1")]
+              var feedback = feedbackTable[mutableListOf(exName, keypoint, "U1")]
               return Utils.CustomFeedbackClass(
-                  feedback?.get(0) ?: "No Feedback",
-                  feedback?.get(1) ?: "0.mp3",
+                  feedback?.get(0),
+                  feedback?.get(1),
                   isFormDetected = true,
                   isFormCorrect = false
               )
           }
           if (maxAngle > (maxIdealAngle - Constants.FEEDBACK_EASINESS)) { // U2
-              val feedback = feedbackTable[mutableListOf(exName, keypoint, "U2")]
+              var feedback = feedbackTable[mutableListOf(exName, keypoint, "U2")]
               return Utils.CustomFeedbackClass(
-                feedback?.get(0) ?: "No Feedback",
-                feedback?.get(1) ?: "0.mp3",
+                feedback?.get(0),
+                feedback?.get(1),
                 isFormDetected = true,
                 isFormCorrect = false
               )
